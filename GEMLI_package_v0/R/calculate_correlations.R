78843963c43350951c7f8d71b029bea3479dbfea
Edoardo Giacopuzzi <51458073+edg1983@users.noreply.github.com>
2024-09-12 14:31:13 +0200

    Add log to calculate_correlations.R

--- GEMLI_package_v0/R/calculate_correlations.R
+++ GEMLI_package_v0/R/calculate_correlations.R
@@ -1,7 +1,9 @@
 calculate_correlations <- function(data, fast = FALSE)
 {
+  message("Compute correlations")
   tmp = t(apply(data, 1, rank)); tmp = as.matrix(tmp); tmp = tmp - rowMeans(tmp); tmp = tmp / sqrt(rowSums(tmp^2))
   if(fast == TRUE){
+  message("Using fast method")
   r = fastCor(t(tmp), nSplit = 10, upperTri = TRUE, optBLAS = TRUE, verbose=FALSE)}
   else{
   r = tcrossprod(tmp)}
