66d2f3a663ba611a927c52f2dc348112805ac25c
Edoardo Giacopuzzi <51458073+edg1983@users.noreply.github.com>
2024-09-12 16:30:59 +0200

    parallelize iterations in predict_lineages.R

--- GEMLI_package_v0/R/predict_lineages.R
+++ GEMLI_package_v0/R/predict_lineages.R
@@ -1,12 +1,21 @@
-predict_lineages <- function(GEMLI_items, repetitions=100, sample_size=(2/3), desired_cluster_size=c(2,3), fast=FALSE) # check
+predict_lineages <- function(GEMLI_items, repetitions=100, sample_size=(2/3), desired_cluster_size=c(2,3), fast=FALSE, cores = 0) # check
 {
+  ncores <- cores
+  if (ncores == 0) {
+    ncores <- detectCores(logical = FALSE)
+  }
+  
+  cl <- makeCluster(ncores)
+  registerDoParallel(cl)
+
   data_matrix = GEMLI_items[['gene_expression']]
   marker_genes = potential_markers(data_matrix)
-  results = data.matrix(matrix(0, nrow=ncol(data_matrix), ncol=ncol(data_matrix))); rownames(results) = colnames(data_matrix); colnames(results) = colnames(data_matrix)
+  
   message("Predict lineages using ", repetitions, " repetitions")
-  for (i in seq(1,repetitions))
+  results <- foreach(i = 1:repetitions, .combine = "+", .packages=c("GEMLI")) %dopar%
   {
     message("##### STEP ", i, " of ", repetitions, " #####")
+    r = data.matrix(matrix(0, nrow=ncol(data_matrix), ncol=ncol(data_matrix))); rownames(results) = colnames(data_matrix); colnames(results) = colnames(data_matrix)
     marker_genes_sample = sample(intersect(marker_genes, rownames(data_matrix)), round(length(intersect(marker_genes, rownames(data_matrix)))*sample_size,0))
     cell_clusters = quantify_clusters_iterative(data_matrix, marker_genes_sample, N=2, fast)
     cell_clusters_unique_name = cell_clusters; for (colname in 1:ncol(cell_clusters)){cell_clusters_unique_name[!is.na(cell_clusters_unique_name[,colname]),colname] = paste0(colname,'_',cell_clusters_unique_name[!is.na(cell_clusters_unique_name[,colname]),colname])}
@@ -15,8 +24,10 @@ predict_lineages <- function(GEMLI_items, repetitions=100, sample_size=(2/3), de
     best_prediction = data.matrix(matrix(F, nrow=ncol(data_matrix), ncol=ncol(data_matrix))); rownames(best_prediction) = colnames(data_matrix); colnames(best_prediction) = colnames(data_matrix)
     for (cluster in smallest_clusters){cells_in_cluster = rownames(best_prediction)[rowSums(cell_clusters_unique_name==cluster, na.rm=T)>0]; best_prediction[cells_in_cluster,cells_in_cluster] <- T}
     diag(best_prediction) = F
-    results = results + best_prediction
+    r = r + best_prediction
+    r
   }
+  stopCluster(cl)
   GEMLI_items[["prediction"]] = results
   return(GEMLI_items)
 }
